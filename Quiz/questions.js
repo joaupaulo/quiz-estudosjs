e88ad2fc29215db436d8bfb988b97bcdf6b830f1
joaupaulo <paulosantos1799@outlook.com>
2020-12-20 14:55:18 -0300

    primeiro mit

---
+++ Quiz/questions.js
@@ -0,0 +1,54 @@
+const askData = [
+    
+    
+    {
+        question : 'Quantos titulos o F.C CLUBE BAHIA TEM ? ' ,
+        a : '78' ,
+        b : ' 90 ' ,
+        c : '54' ,
+        d : '89',
+        correta : 'D'
+} ,
+
+{
+    question : 'Qual a maior cidade da america látina ? ' ,
+    a : 'São Paulo' ,
+    b : ' Rio De Janeiro ' ,
+    c : 'Belo Horizonte' ,
+    d : 'Salvador',
+    correta : 'A'
+} ,
+
+{
+    question : 'Qual a moeda mas cara dentre essas ? ' ,
+    a : 'Dolar' ,
+    b : ' Dolar Canadense' ,
+    c : 'Euro' ,
+    d : 'Real',
+    correta : 'C'
+} ,
+
+
+
+{
+    question : 'Qual a moeda mas cara dentre essas ? ' ,
+    a : 'Dolar' ,
+    b : ' Dolar Canadense' ,
+    c : 'Euro' ,
+    d : 'Real',
+    correta : 'C'
+}  ,
+
+
+{
+    question : 'Qual o maior cantode arrocha do momento ? ' ,
+    a : 'Thierry' ,
+    b : 'Luan Santana ' ,
+    c : 'Pablo' ,
+    d : 'Filipe Ret',
+    correta : 'A'
+} 
+ 
+
+
+]
---
+++ index.js
@@ -0,0 +1,9 @@
+
+var vermelho = document.querySelector('.teste')
+
+function troca () {
+
+    vermelho.style.backgroundColor="blue"; 
+
+
+}
